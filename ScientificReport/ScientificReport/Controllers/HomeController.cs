9a4e41a3e7c57d01a48d8f06de1f3a34f7f88209
Volodymyr <35489438+GuziyV@users.noreply.github.com>
2019-03-28 21:21:16 +0200

    Move enums to separate files

--- ScientificReport/ScientificReport/Controllers/HomeController.cs
+++ ScientificReport/ScientificReport/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ScientificReport.Models;
 
 namespace ScientificReport.Controllers
 {
@@ -13,17 +12,6 @@ namespace ScientificReport.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- ScientificReport/ScientificReport/Data/Enums/Role.cs
+++ ScientificReport/ScientificReport/Data/Enums/Role.cs
@@ -3,20 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ScientificReport.Data.Models
+namespace ScientificReport.Data.Enums
 {
     public enum Role
     {
         Teacher,
         DepartmentHead,
         Dean
-    }
-
-    public enum PublicationStatus
-    {
-        Published,
-        Printed,
-        InProgres,
-        InReview
     }
 }

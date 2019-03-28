0e392a07052be7da4d1972d29963eec5494c225b
Bohdan Khymera <bogdankhymera@gmail.com>
2019-03-28 23:23:21 +0200

    Report services. Create report action. Models extended

---
+++ ScientificReport/ScientificReport/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScientificReport.Data.Models;
+
+namespace ScientificReport.Services
+{
+    public class UserService : IUserService
+    {
+        public User CurrentUser { get; set; }
+    }
+}

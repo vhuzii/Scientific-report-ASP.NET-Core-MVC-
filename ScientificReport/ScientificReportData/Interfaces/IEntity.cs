26e29665b707590f49de003e40a184ada937bcd1
Volodymyr <35489438+GuziyV@users.noreply.github.com>
2019-04-24 22:43:39 +0300

    Ad sum

--- ScientificReport/ScientificReportData/Interfaces/IEntity.cs
+++ ScientificReport/ScientificReportData/Interfaces/IEntity.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace ScientificReportData.Interfaces 
 {
+	/// <summary>
+	/// Інтерфейс для моделі
+	/// </summary>
+	/// <typeparam name="T">Тип ідентифікатора</typeparam>
 	public interface IEntity<T>
 	{
 		T Id { get; set; }
